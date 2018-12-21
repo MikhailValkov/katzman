37e8ec09f626292347daead0ee67039cf5407340
Mikhail Valkov <mikhail.valkov@gmail.com>
2018-12-21 11:51:27 +0300

    Готова станица -о нас-

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,17 +1,6 @@
 function showAbout() {
   let aboutBlock = document.getElementById("about");
-  // let aboutBtn = document.getElementById("about-btn");
-  // if (aboutBtn.textContent === "О нас") {
-  // console.log("show about");
   aboutBlock.style.left = "0px";
-  // aboutBtn.style.backgroundColor = "rgba(255, 50, 50, 0.8)";
-  // aboutBtn.textContent = "Закрыть";
-  // } else {
-  // console.log("close about");
-
-  // aboutBtn.textContent = "О нас";
-  // aboutBtn.style.backgroundColor = "rgba(255,255,255,0.8)";
-  // }
 }
 
 function closeAbout() {
