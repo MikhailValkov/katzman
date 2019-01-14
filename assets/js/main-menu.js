231f0939e5addf1af160a7f0b25332c575678beb
Mikhail Valkov <mikhail.valkov@gmail.com>
2019-01-14 11:40:20 +0300

    готово главное меню

--- assets/js/main-menu.js
+++ assets/js/main-menu.js
@@ -1,9 +1,15 @@
 function showMainMenu() {
-  let aboutBlock = document.getElementById("main-menu");
-  aboutBlock.style.left = "0px";
+  let menuBlock = document.getElementById("main-menu");
+  let menuButton = document.getElementById("menu-button");
+  menuBlock.style.left = "0px";
+  menuButton.innerHTML = "Закрыть";
+  menuButton.onclick = closeMainMenu;
 }
 
 function closeMainMenu() {
-  let aboutBlock = document.getElementById("main-menu");
-  aboutBlock.style.left = "-100vw";
+  let menuBlock = document.getElementById("main-menu");
+  let menuButton = document.getElementById("menu-button");
+  menuBlock.style.left = "-100vw";
+  menuButton.innerHTML = "Меню";
+  menuButton.onclick = showMainMenu;
 }
