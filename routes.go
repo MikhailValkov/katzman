c247c50ed0a642731d4f260649294c86861c400a
Mikhail Valkov <mikhail.valkov@gmail.com>
2019-01-30 13:18:31 +0300

    добавил заготовку под металлосайдинг

--- routes.go
+++ routes.go
@@ -14,6 +14,7 @@ func GetRoutes() *http.ServeMux {
 	mux.HandleFunc("/", Chain(views.IndexHandler, Method("GET"), Logging()))
 	mux.HandleFunc("/мокрый-фасад", Chain(views.MokrijFasadHandler, Method("GET"), Logging()))
 	mux.HandleFunc("/виниловый-сайдинг", Chain(views.VinilHandler, Method("GET"), Logging()))
+	mux.HandleFunc("/металлосайдинг", Chain(views.MetallHandler, Method("GET"), Logging()))
 	// mux.HandleFunc("/admin", Chain(views.GetCalcData, Method("GET"), Logging(), SessionChecker()))
 
 	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets/"))))
---
+++ views/metall.go
@@ -0,0 +1,38 @@
+package views
+
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/meehalkoff/loggi"
+)
+
+func MetallHandler(w http.ResponseWriter, r *http.Request) {
+
+	tmpl := template.New("metall")
+	tmpl.ParseFiles(
+		"templates/header.html",
+		"templates/footer.html",
+		"templates/top-panel/top-panel.html",
+		"templates/top-panel/about.html",
+		"templates/top-panel/calc.html",
+		"templates/top-panel/call.html",
+		"templates/top-panel/main-menu.html",
+		"templates/control-panel.html",
+		"templates/metall/index.html",
+		"templates/metall/summary.html",
+		"templates/metall/palitra.html",
+		"templates/metall/kachestva.html",
+		"templates/metall/schema.html",
+		"templates/metall/otkos.html",
+		"templates/metall/portfolio.html",
+		"templates/metall/video.html",
+		"templates/metall/promo.html",
+		"templates/metall/qualities.html",
+		"templates/metall/we-work.html",
+	)
+
+	if err := tmpl.Execute(w, nil); err != nil {
+		loggi.Fatal(err)
+	}
+}
