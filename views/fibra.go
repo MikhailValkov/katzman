88d980c5a86ec44df05839eecb82f96d80f4acbc
Mikhail Valkov <mikhail.valkov@gmail.com>
2019-03-04 10:28:48 +0300

    готова мобильная фиброцемента

--- views/fibra.go
+++ views/fibra.go
@@ -4,31 +4,46 @@ import (
 	"html/template"
 	"net/http"
 
+	mobiledetect "github.com/Shaked/gomobiledetect"
 	"github.com/meehalkoff/loggi"
 )
 
 func FibraHandler(w http.ResponseWriter, r *http.Request) {
 
 	tmpl := template.New("fibra")
-	tmpl.ParseFiles(
-		"templates/header.html",
-		"templates/footer.html",
-		"templates/top-panel/top-panel.html",
-		"templates/top-panel/about.html",
-		"templates/top-panel/calc.html",
-		"templates/top-panel/call.html",
-		"templates/top-panel/main-menu.html",
-		"templates/control-panel.html",
-		"templates/fibra/index.html",
-		"templates/fibra/summary.html",
-		"templates/fibra/palitra.html",
-		"templates/fibra/kachestva.html",
-		"templates/fibra/schema.html",
-		"templates/fibra/otkos.html",
-		"templates/fibra/portfolio.html",
-		"templates/fibra/video.html",
-		"templates/fibra/promo.html",
-	)
+
+	detect := mobiledetect.NewMobileDetect(r, nil)
+	if detect.IsMobile() {
+		tmpl.ParseFiles(
+			"mobile/templates/fibra.html",
+			"mobile/templates/header.html",
+			"mobile/templates/footer.html",
+			"mobile/templates/top-panel.html",
+			"mobile/templates/calc.html",
+			"mobile/templates/call.html",
+			"mobile/templates/about.html",
+		)
+	} else {
+		tmpl.ParseFiles(
+			"templates/header.html",
+			"templates/footer.html",
+			"templates/top-panel/top-panel.html",
+			"templates/top-panel/about.html",
+			"templates/top-panel/calc.html",
+			"templates/top-panel/call.html",
+			"templates/top-panel/main-menu.html",
+			"templates/control-panel.html",
+			"templates/fibra/index.html",
+			"templates/fibra/summary.html",
+			"templates/fibra/palitra.html",
+			"templates/fibra/kachestva.html",
+			"templates/fibra/schema.html",
+			"templates/fibra/otkos.html",
+			"templates/fibra/portfolio.html",
+			"templates/fibra/video.html",
+			"templates/fibra/promo.html",
+		)
+	}
 
 	if err := tmpl.Execute(w, nil); err != nil {
 		loggi.Fatal(err)
