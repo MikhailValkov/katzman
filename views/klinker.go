c8ea95c4933f52061896bbe2871b58376f765bc0
Mikhail Valkov <mikhail.valkov@gmail.com>
2019-03-04 12:10:25 +0300

    готова мобильная клинкер

--- views/klinker.go
+++ views/klinker.go
@@ -4,31 +4,46 @@ import (
 	"html/template"
 	"net/http"
 
+	mobiledetect "github.com/Shaked/gomobiledetect"
 	"github.com/meehalkoff/loggi"
 )
 
 func KlinkerHandler(w http.ResponseWriter, r *http.Request) {
 
 	tmpl := template.New("klinker")
-	tmpl.ParseFiles(
-		"templates/header.html",
-		"templates/footer.html",
-		"templates/top-panel/top-panel.html",
-		"templates/top-panel/about.html",
-		"templates/top-panel/calc.html",
-		"templates/top-panel/call.html",
-		"templates/top-panel/main-menu.html",
-		"templates/control-panel.html",
-		"templates/klinker/index.html",
-		"templates/klinker/summary.html",
-		"templates/klinker/palitra.html",
-		"templates/klinker/kachestva.html",
-		"templates/klinker/schema.html",
-		"templates/klinker/otkos.html",
-		"templates/klinker/portfolio.html",
-		"templates/klinker/video.html",
-		"templates/klinker/promo.html",
-	)
+
+	detect := mobiledetect.NewMobileDetect(r, nil)
+	if detect.IsMobile() {
+		tmpl.ParseFiles(
+			"mobile/templates/klinker.html",
+			"mobile/templates/header.html",
+			"mobile/templates/footer.html",
+			"mobile/templates/top-panel.html",
+			"mobile/templates/calc.html",
+			"mobile/templates/call.html",
+			"mobile/templates/about.html",
+		)
+	} else {
+		tmpl.ParseFiles(
+			"templates/header.html",
+			"templates/footer.html",
+			"templates/top-panel/top-panel.html",
+			"templates/top-panel/about.html",
+			"templates/top-panel/calc.html",
+			"templates/top-panel/call.html",
+			"templates/top-panel/main-menu.html",
+			"templates/control-panel.html",
+			"templates/klinker/index.html",
+			"templates/klinker/summary.html",
+			"templates/klinker/palitra.html",
+			"templates/klinker/kachestva.html",
+			"templates/klinker/schema.html",
+			"templates/klinker/otkos.html",
+			"templates/klinker/portfolio.html",
+			"templates/klinker/video.html",
+			"templates/klinker/promo.html",
+		)
+	}
 
 	if err := tmpl.Execute(w, nil); err != nil {
 		loggi.Fatal(err)
