4ffa97fdbc851ba27f3eb699fb5568e3ec464829
Mikhail Valkov <m@valkov.gq>
2020-05-18 15:41:09 +0300

    убрал лишние поля в  форме заказа звонка

--- views/callme.go
+++ views/callme.go
@@ -11,8 +11,8 @@ import (
 )
 
 type CallData struct {
-	Name  string `json:"name"`
-	Email string `json:"email"`
+	// Name  string `json:"name"`
+	// Email string `json:"email"`
 	Phone string `json:"phone"`
 }
 
@@ -26,7 +26,8 @@ func CallMeHandler(w http.ResponseWriter, r *http.Request) {
 	data := CallData{}
 	json.Unmarshal(b, &data)
 
-	msg := strings.Replace(fmt.Sprintf("Заказ замера: %s+%s+%s", data.Name, data.Email, data.Phone), " ", "+", -1)
+	// msg := strings.Replace(fmt.Sprintf("Заказ замера: %s+%s+%s", data.Name, data.Email, data.Phone), " ", "+", -1)
+	msg := strings.Replace(fmt.Sprintf("Заказ замера: %s", data.Phone), " ", "+", -1)
 	url := fmt.Sprintf("https://sms.ru/sms/send?api_id=A2C2432C-8573-0446-66E2-9F2E925F08FE&to=79045345759&msg=%s", msg)
 	// loggi.Info(msg)
 
