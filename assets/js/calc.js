3ab51c7507f598bdc191f8d59e672d29d079be71
Mikhail Valkov <mikhail.valkov@gmail.com>
2020-09-04 16:29:09 +0300

    сменил логику калькулятора и поправил цены

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -1,78 +1,242 @@
 var materials = {
-    "decor": {
-        "koroed": {
-            "cost": 240,
-            "job_cost": 500,
-            "warm_coef": 1
-        },
-        "mramor": {
-            "cost": 762,
-            "job_cost": 500,
-            "warm_coef": 1
-        },
-        "hauberg": {
-            "cost": 350,
-            "job_cost": 400,
-            "warm_coef": .35
-        },
-        "vinil": {
-            "cost": 450,
-            "job_cost": 360,
-            "warm_coef": .35
-        },
-        "metal": {
-            "cost": 530,
-            "job_cost": 400,
-            "warm_coef": .35
-        },
-        "fibra": {
-            "cost": 1900,
-            "job_cost": 610,
-            "warm_coef": .35
-        },
-        "termo": {
-            "cost": 850,
-            "job_cost": 400,
-            "warm_coef": 0
-        },
-        "klinker": {
-            "cost": 1750,
-            "job_cost": 750,
-            "warm_coef": 0
-        },
-        "decor": {
-            "cost": 560,
-        }
-    },
-    "warm": {
-        "nowarm": 0,
-        "penoplast": {
-            "dsoft": 413,
-            "dmid": 468,
-            "dhard": 628
-        },
-        "penopleks": {
-            "dsoft": 485,
-            "dmid": 597,
-            "dhard": 887
+    // "decor": {
+    "koroed": {
+        "cost": 360,
+        "job_cost": 500,
+        "warm_coef": 1,
+        // "warm": {
+        "nowarm": {
+            "dsoft": 0,
+            "dmid": 0,
+            "dhard": 0
         },
         "vata": {
-            "dsoft": 506,
-            "dmid": 608,
-            "dhard": 908
-        }
+            "dsoft": 613,
+            "dmid": 728,
+            "dhard": 1050
+        },
+        "penoplast": {
+            "dsoft": 516,
+            "dmid": 571,
+            "dhard": 736
+        },
+        "penopleks": {
+            "dsoft": 566,
+            "dmid": 649,
+            "dhard": 892
+        },
+        // }
+    },
+    "mramor": {
+        "cost": 705,
+        "job_cost": 600,
+        "warm_coef": 1,
+        "nowarm": {
+            "dsoft": 0,
+            "dmid": 0,
+            "dhard": 0
+        },
+        "vata": {
+            "dsoft": 563,
+            "dmid": 678,
+            "dhard": 1000
+        },
+        "penoplast": {
+            "dsoft": 466,
+            "dmid": 521,
+            "dhard": 686
+        },
+        "penopleks": {
+            "dsoft": 516,
+            "dmid": 599,
+            "dhard": 842
+        },
+    },
+    "hauberg": {
+        "cost": 970,
+        "job_cost": 500,
+        "warm_coef": 1,
+        "nowarm": {
+            "dsoft": 0,
+            "dmid": 0,
+            "dhard": 0
+        },
+        "vata": {
+            "dsoft": 180,
+            "dmid": 230,
+            "dhard": 330
+        },
+        "penoplast": {
+            "dsoft": 230,
+            "dmid": 280,
+            "dhard": 430
+        },
+        "penopleks": {
+            "dsoft": 260,
+            "dmid": 330,
+            "dhard": 580
+        },
+    },
+    "vinil": {
+        "cost": 400,
+        "job_cost": 300,
+        "warm_coef": 1,
+        "nowarm": {
+            "dsoft": 0,
+            "dmid": 0,
+            "dhard": 0
+        },
+        "vata": {
+            "dsoft": 176,
+            "dmid": 250,
+            "dhard": 350
+        },
+        "penoplast": {
+            "dsoft": 250,
+            "dmid": 300,
+            "dhard": 450
+        },
+        "penopleks": {
+            "dsoft": 300,
+            "dmid": 350,
+            "dhard": 600
+        },
+    },
+    "metal": {
+        "cost": 630,
+        "job_cost": 300,
+        "warm_coef": 1,
+        "nowarm": {
+            "dsoft": 0,
+            "dmid": 0,
+            "dhard": 0
+        },
+        "vata": {
+            "dsoft": 170,
+            "dmid": 220,
+            "dhard": 320
+        },
+        "penoplast": {
+            "dsoft": 220,
+            "dmid": 270,
+            "dhard": 450
+        },
+        "penopleks": {
+            "dsoft": 270,
+            "dmid": 370,
+            "dhard": 600
+        },
+    },
+    "fibra": {
+        "cost": 1380,
+        "job_cost": 300,
+        "warm_coef": 1,
+        "nowarm": {
+            "dsoft": 0,
+            "dmid": 0,
+            "dhard": 0
+        },
+        "vata": {
+            "dsoft": 180,
+            "dmid": 220,
+            "dhard": 340
+        },
+        "penoplast": {
+            "dsoft": 220,
+            "dmid": 280,
+            "dhard": 450
+        },
+        "penopleks": {
+            "dsoft": 270,
+            "dmid": 370,
+            "dhard": 570
+        },
+    },
+    "termo": {
+        "cost": 850,
+        "job_cost": 400,
+        "warm_coef": 0,
+        "nowarm": {
+            "dsoft": 0,
+            "dmid": 0,
+            "dhard": 0
+        },
+        "vata": {
+            "dsoft": 0,
+            "dmid": 0,
+            "dhard": 0
+        },
+        "penoplast": {
+            "dsoft": 0,
+            "dmid": 0,
+            "dhard": 0
+        },
+        "penopleks": {
+            "dsoft": 0,
+            "dmid": 0,
+            "dhard": 0
+        },
+    },
+    "klinker": {
+        "cost": 2300,
+        "job_cost": 0,
+        "warm_coef": 0,
+        "nowarm": {
+            "dsoft": 0,
+            "dmid": 0,
+            "dhard": 0
+        },
+        "vata": {
+            "dsoft": 0,
+            "dmid": 0,
+            "dhard": 0
+        },
+        "penoplast": {
+            "dsoft": 0,
+            "dmid": 0,
+            "dhard": 0
+        },
+        "penopleks": {
+            "dsoft": 0,
+            "dmid": 0,
+            "dhard": 0
+        },
+
+    },
+    "decor": {
+        "cost": 560,
     }
+    // },
+    // "warm": {
+    //     "nowarm": 0,
+    //     "penoplast": {
+    //         "dsoft": 413,
+    //         "dmid": 468,
+    //         "dhard": 628
+    //     },
+    //     "penopleks": {
+    //         "dsoft": 485,
+    //         "dmid": 597,
+    //         "dhard": 887
+    //     },
+    //     "vata": {
+    //         "dsoft": 506,
+    //         "dmid": 608,
+    //         "dhard": 908
+    //     }
+    // }
 }
 
 var defaultSetting = {
     "decor": "koroed",
     "warm": "nowarm",
+    "depth": "dsoft"
 }
 
 var calcState = {
     "decor": "koroed",
     "warm": "nowarm",
-    "depth": "",
+    "depth": "dsoft",
     "square": 1,
 }
 
@@ -118,7 +282,7 @@ function controlDepth(btn) {
 
 function controlWarm(btn) {
     try {
-        var warm_coef = materials.decor[btn.id].warm_coef;
+        var warm_coef = materials[btn.id].warm_coef;
 
         if (warm_coef > 0) {
             enableWarm()
@@ -216,7 +380,6 @@ function setCalcState(btn) {
         "vata",
     ];
     var depthNames = [
-        "",
         "dsoft",
         "dmid",
         "dhard"
@@ -226,7 +389,7 @@ function setCalcState(btn) {
     }
     if (warmNames.indexOf(btn.id) > -1) {
         if (btn.id === "nowarm") {
-            calcState.depth = "";
+            calcState.depth = "dsoft";
         }
         calcState.warm = btn.id;
     }
@@ -242,18 +405,28 @@ function checkInput(inp) {
 
 function calculateSumm() {
     var out = document.getElementById("calcSumma");
-    var cost = materials.decor[calcState.decor].cost;
-    var job_cost = materials.decor[calcState.decor].job_cost;
-    var warm_coef = materials.decor[calcState.decor].warm_coef;
 
-    var cost_warm = 0;
-    if (calcState.depth !== "") {
-        cost_warm = materials.warm[calcState.warm][calcState.depth];
-    }
+    // var cost = materials.decor[calcState.decor].cost;
+    // var job_cost = materials.decor[calcState.decor].job_cost;
+    // var warm_coef = materials.decor[calcState.decor].warm_coef;
 
-    var square = calcState.square;
+    // var cost_warm = 0;
+    // if (calcState.depth !== "") {
+    //     cost_warm = materials.warm[calcState.warm][calcState.depth];
+    // }
 
-    var result = ((cost + job_cost) + (cost_warm * warm_coef)) * square;
+    // var square = calcState.square;
+
+    // var result = ((cost + job_cost) + (cost_warm * warm_coef)) * square;
+
+
+    let decorCost = materials[calcState.decor].cost;
+    let decorJobCost = materials[calcState.decor].job_cost;
+    let warmCost = materials[calcState.decor][calcState.warm][calcState.depth];
+
+    let result = (decorCost + decorJobCost + warmCost) * calcState.square;
+
+
     console.log(result);
     out.value = Math.round(result);
 }
--- assets/js/calculateMainContentPrice.js
+++ assets/js/calculateMainContentPrice.js
@@ -1,56 +1,56 @@
 
 try {
     var mPrice = document.getElementById("mokrijPrice");
-    mPrice.innerText = materials.decor.koroed.cost + materials.decor.koroed.job_cost;
+    mPrice.innerText = materials.koroed.cost + materials.koroed.job_cost;
 } catch (err) {
     console.log(err)
 }
 
 try {
     var hPrice = document.getElementById("haubergPrice");
-    hPrice.innerText = materials.decor.hauberg.cost + materials.decor.hauberg.job_cost;
+    hPrice.innerText = materials.hauberg.cost + materials.hauberg.job_cost;
 } catch (err) {
     console.log(err)
 }
 
 try {
     var vPrice = document.getElementById("vinilPrice");
-    vPrice.innerText = materials.decor.vinil.cost + materials.decor.vinil.job_cost;
+    vPrice.innerText = materials.vinil.cost + materials.vinil.job_cost;
 } catch (err) {
     console.log(err)
 }
 
 try {
     var mePrice = document.getElementById("metalPrice");
-    mePrice.innerText = materials.decor.metal.cost + materials.decor.metal.job_cost;
+    mePrice.innerText = materials.metal.cost + materials.metal.job_cost;
 } catch (err) {
     console.log(err)
 }
 
 try {
     var fPrice = document.getElementById("fibraPrice");
-    fPrice.innerText = materials.decor.fibra.cost + materials.decor.fibra.job_cost;
+    fPrice.innerText = materials.fibra.cost + materials.fibra.job_cost;
 } catch (err) {
     console.log(err)
 }
 
 try {
     var tPrice = document.getElementById("termoPrice");
-    tPrice.innerText = materials.decor.termo.cost + materials.decor.termo.job_cost;
+    tPrice.innerText = materials.termo.cost + materials.termo.job_cost;
 } catch (err) {
     console.log(err)
 }
 
 try {
     var kPrice = document.getElementById("klinkerPrice");
-    kPrice.innerText = materials.decor.klinker.cost + materials.decor.klinker.job_cost;
+    kPrice.innerText = materials.klinker.cost + materials.klinker.job_cost;
 } catch (err) {
     console.log(err)
 }
 
 try {
     var dPrice = document.getElementById("decorPrice");
-    dPrice.innerHTML = materials.decor.decor.cost;
+    dPrice.innerHTML = materials.decor.cost;
 } catch (err) {
     console.log(err)
 }
